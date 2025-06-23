2b784348b21caaa6ceb2139f9df22eb243469ee9
DeaRozze <vl4dkudi@gmail.com>
2025-06-23 16:59:03 +0300

    feat: сделал новый компонент окна авторизации, сделал начальную разметку

---
+++ src/components/UI/AuthModal.vue
@@ -0,0 +1,56 @@
+<script setup>
+import AppModal from './AppModal.vue'
+</script>
+
+<template>
+  <AppModal>
+    <h2 class="auth-modal__title"></h2>
+    <form class="auth-modal__form">
+      <div class="auth-modal__form-group">
+        <label
+          for="auth-name"
+          class="auth-modal__label"
+          >Имя:</label
+        >
+        <input type="text" />
+      </div>
+
+      <div class="auth-modal__form-group">
+        <label
+          for="auth-password"
+          class="auth-modal__label"
+          >Пароль:</label
+        >
+        <input type="text" />
+      </div>
+
+      <div class="auth-modal__form-group">
+        <label
+          for="auth-avatar"
+          class="auth-modal__label"
+          >Аватар:</label
+        >
+        <input type="file" />
+        <div class="auth-modal__avatar-preview">
+          <img
+            :src="avatar"
+            alt="Аватар"
+            class="auth-modal__avatar-image"
+          />
+        </div>
+      </div>
+      <div class="auth-modal__error"></div>
+      <AppButton
+        type="submit"
+        class="auth-modal__submit"
+      >
+        {{ isLoginMode ? 'Войти' : 'Зарегистрироваться' }}
+      </AppButton>
+      <button>
+        {{ isLoginMode ? 'Нет аккаунта? Зарегистрироваться' : 'Уже есть аккаунт? Войти' }}
+      </button>
+    </form>
+  </AppModal>
+</template>
+
+<style lang="scss" scoped></style>

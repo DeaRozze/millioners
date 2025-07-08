671057ef1063a74499ab41aafc0c735f083e74f2
DeaRozze <vl4dkudi@gmail.com>
2025-07-08 18:34:53 +0300

    fix: изменил конфиг eslint под ts

--- eslint.config.js
+++ eslint.config.js
@@ -7,47 +7,54 @@ import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 
 export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'],
-  },
-
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   {
+    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'],
     languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        extraFileExtensions: ['.vue'],
-      },
       globals: {
         ...globals.browser,
       },
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
   },
 
   js.configs.recommended,
+
   ...pluginVue.configs['flat/essential'],
 
   {
+    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+    languageOptions: {
+      parser: pluginVue.parser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+        project: './tsconfig.json',
+      },
+    },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       vue: pluginVue,
     },
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+
+  {
+    files: ['**/*.vue'],
     rules: {
       'vue/max-attributes-per-line': [
         'error',
         {
-          singleline: {
-            max: 1,
-          },
-          multiline: {
-            max: 1,
-          },
+          singleline: { max: 1 },
+          multiline: { max: 1 },
         },
       ],
     },
